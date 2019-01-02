9470e22db16c67c4f05e5fcbf5f28f2388411178
adonis <adonis.lau.work@gmail.com>
2019-01-02 20:26:43 +0800

    工具方法测试

--- hadoop-demo/src/test/java/bid/adonis/lau/test/Hadoop.java
+++ hadoop-demo/src/test/java/bid/adonis/lau/test/Hadoop.java
@@ -11,6 +11,7 @@ import org.apache.hadoop.fs.RemoteIterator;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -67,11 +68,16 @@ public class Hadoop {
             return true;
         };
 
-        Path path = new Path("/hbase/data/hbase/namespace/da3b71f05bd087ff18f324fe86574153/recovered.edits");
+//        Path path = new Path("/hbase/data/hbase/namespace/da3b71f05bd087ff18f324fe86574153/recovered.edits");
+        Path path = new Path("/learn/test-files/word-count/words.txt");
+        FileStatus[] fileStatuses = fs.globStatus(path);
+        System.out.println(fileStatuses.length);
+        System.out.println(fileStatuses[0]);
         List<FileStatus> children = getChildren(path.toString(), pathFilter);
-        for (FileStatus child : children) {
-            System.out.println(child.getPath().toUri().toString());
+        if (isDirectory(path.toString())) {
+            System.out.println("is directory");
         }
+//        System.out.println(children.size() < 1);
 
     }
 
@@ -82,7 +88,7 @@ public class Hadoop {
         */
 
 
-        FileStatus[] fileStatuses = fs.globStatus(new Path(hdfsPath), pathFilter);
+        FileStatus[] fileStatuses = fs.listStatus(new Path(hdfsPath), pathFilter);
         List<FileStatus> retList = new ArrayList<FileStatus>();
         for (int i = 0; i < fileStatuses.length; i++) {
             List<FileStatus> list = getChildren(fileStatuses[i], pathFilter);
@@ -107,4 +113,51 @@ public class Hadoop {
         return retList;
     }
 
+
+    /**
+     * 根据hdfs路径获得路径的状态
+     * num == 1 ，不是目录
+     *
+     * @param hdfsPath
+     * @return
+     * @throws java.io.IOException
+     */
+    public int getFileStatus(String hdfsPath) throws IOException {
+        FileStatus[] fileStatus = fs.globStatus(new Path(hdfsPath));
+        if (fileStatus == null || fileStatus.length == 0) {
+            throw new FileNotFoundException("Cannot access " + hdfsPath + ": No such file or directory.");
+        }
+        int num = 0;
+        for (int i = 0; i < fileStatus.length; i++) {
+            num += getFileStatus(fileStatus[i]);
+        }
+        return num;
+    }
+
+    /**
+     * 判断hdfs路径是否是目录
+     *
+     * @param hdfsPath
+     * @return
+     * @throws java.io.IOException
+     */
+    public boolean isDirectory(String hdfsPath) throws IOException {
+        int num = getFileStatus(hdfsPath);
+        return num > 0;
+    }
+
+    private int getFileStatus(FileStatus fileStatus) throws IOException {
+        //hadoopVFS.LOGGER.logDebug("status:" + fileStatus.getPath());
+        FileStatus[] fileStatuses = fs.listStatus(fileStatus.getPath());
+        //hadoopVFS.LOGGER.logDebug(fileStatuses == null ? "null" : "not null"+fileStatuses.length);
+        if (fileStatuses.length == 1 && !fileStatuses[0].isDirectory()) {
+            //hadoopVFS.LOGGER.logDebug("fileStatus[0]"+ fileStatuses[0].getPath());
+            return 1;
+        }
+        int num = 0;
+        for (int i = 0; i < fileStatuses.length; i++) {
+            num += getFileStatus(fileStatuses[i]);
+        }
+        return num;
+    }
 }
